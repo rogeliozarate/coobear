054a5f9dbe91fbb2445fee04db8a31710a8c1f93
Rogelio Zarate <i.am@rogeliozarate.com>
2013-08-17 21:28:18 -0500

    Define coolbear ASCII var
    - Dump it to console

---
+++ coolbear.js
@@ -0,0 +1,15 @@
+var coolbear =
+  "    ('-^-/')  \n" +
+  "    `o__o' ]  \n" +
+  "    (_Y_) _/  \n" +
+  "  _..`--'-.`, \n" +
+  " (__)_,--(__) \n" +
+  "     7:   ; 1 \n" +
+  "   _/,`-.-' : \n" +
+  "  (_,)-~~(_,) \n"
+  
+setInterval(function() { console.log(coolbear) }, 1000)
+
+setTimeout(function() {
+  setInterval(function() { console.log(coolbear) }, 1000)
+}, 500)
